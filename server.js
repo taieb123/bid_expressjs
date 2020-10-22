75fe63147c07b47bfdf32887a8a89af584533bbe
taiebrekik <taiebrekik6@gmail.com>
2020-10-22 22:23:04 +0100

    add mongoose

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-//Router file
-const bootcamps = require('./routes/bootcamps')
-
+const connectDB =  require('./config/db');
 //Load envi file
 dotenv.config({path:'./config/config.env'});
+
+//Coonect DB
+connectDB();
+
+//Router file
+const bootcamps = require('./routes/bootcamps')
 
 const app =express();
 
@@ -19,4 +23,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`server is runing ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server =  app.listen(PORT, console.log(`server is runing ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Handle rejection 
+
+process.on('unhandledRejection', (err,promise) => {
+    console.log(`Error : ${err.message}`);
+    server.close( () => process.exit(1));
+})
